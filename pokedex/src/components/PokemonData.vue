bd14e7d1f2a88d401eb9d284c47291f4debd008f
gabriel <gabriel@ezlink.global>
2023-07-17 10:46:47 -0300

    feat: adicona o bulba

--- pokedex/src/components/PokemonData.vue
+++ pokedex/src/components/PokemonData.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>
-        <h1>{{ num }} {{ name | upper }}</h1>
-        <small>{{ url }}</small>
+    <div id="pokemon">
+        <div class="card">
+        <div class="card-image">
+            <figure>
+            <img :src="pokemon.front" alt="Placeholder image">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+                <div class="media-content">
+                    <p class="title is-4">{{ num }} - {{ name | upper }}h</p>
+                    <p class="subtitle is-6">{{ pokemon.type }}</p>
+                </div>
+                </div>
+                <div class="content">
+                
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,7 +28,7 @@ export default {
     created: function(){
         axios.get(this.url)
             .then((res) => {
-                this.pokemon.type = res.data.types[0].type;
+                this.pokemon.type = res.data.types[0].type.name;
                 this.pokemon.front = res.data.sprites.front_default;
                 this.pokemon.back = res.data.sprites.back_default;
                 console.log(this.pokemon);
@@ -20,7 +36,11 @@ export default {
     },
     data(){
         return {
-            pokemon: {}
+            pokemon: {
+                type: '',
+                front: '',
+                back: ''
+            }
         }
     },
     props: {
@@ -38,5 +58,7 @@ export default {
 </script>
 
 <style>
-
+#pokemon{
+    margin-top: 2%;
+}
 </style>
