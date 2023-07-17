c5560fa3525a55b25da030fa72b40155cb4e0058
gabriel <gabriel@ezlink.global>
2023-07-17 10:18:21 -0300

    fazendo o push

---
+++ pokedex/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div v-for="(poke, index) in pokemons" :key="index">
+      <PokemonData :name="poke.name" :url="poke.url" :num="index+1"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import PokemonData from './components/PokemonData';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+  created: function() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        console.log("Pegou a lista de pokemons");
+        this.pokemons = res.data.results;
+      })
+  },
+  components: {
+    PokemonData
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ pokedex/src/components/PokemonData.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>{{ num }} {{ name | upper }}</h1>
+        <small>{{ url }}</small>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    created: function(){
+        axios.get(this.url)
+            .then((res) => {
+                this.pokemon.type = res.data.types[0].type;
+                this.pokemon.front = res.data.sprites.front_default;
+                this.pokemon.back = res.data.sprites.back_default;
+                console.log(this.pokemon);
+            })
+    },
+    data(){
+        return {
+            pokemon: {}
+        }
+    },
+    props: {
+        num: Number,
+        name: String,
+        url: String,
+    },
+    filters: {
+        upper: function(value) {
+            const newName = value[0].toUpperCase() + value.slice(1);
+            return newName
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
